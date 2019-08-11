6ec9cd8e3226982451681b7441bb4fa2c7967b2a
chance <chance.krug@gmail.com>
2019-08-10 23:13:35 -0500

    Created temporary landing page while in development.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to my personal website. Currently under construction. Please come back soon to see the finished version!"/>
   </div>
 </template>
 
