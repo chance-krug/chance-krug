4df255ac8a50446cc017523163857530f7ee6a37
chance <chance.krug@gmail.com>
2019-08-11 15:36:59 -0500

    Created navigation bar and logo for the site.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <NavBar></NavBar>
+    <img alt="Vue logo" src="./assets/Logo1.png">
     <HelloWorld msg="Welcome to my personal website. Currently under construction. Please come back soon to see the finished version!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Vue from 'vue';
+  import 'bootstrap';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import BootstrapVue from 'bootstrap-vue';
+  import HelloWorld from './components/HelloWorld.vue'
+  import NavBar from "@/components/NavBar";
+
+
+  Vue.use(BootstrapVue);
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavBar, HelloWorld
+
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+<!--    &lt;!&ndash; Just an image &ndash;&gt;-->
+<!--    <nav class="navbar navbar-light bg-light fixed-top">-->
+<!--        <a class="navbar-brand" href="#">-->
+<!--            <img src="@/assets/Logo1.png" width="40" height="40" alt="">-->
+<!--        </a>-->
+<!--        <form class="form-inline">-->
+<!--            <button class="btn btn-outline-success" type="button">Contact Me</button>-->
+<!--        </form>-->
+<!--    </nav>-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="#">
+            <img src="@/assets/Logo1.png" width="40" height="40" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Education</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Experience</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Skills</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Projects</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+<script>
+</script>
+
+<style scoped>
+
+</style>
